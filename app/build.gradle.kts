dc4583724c643089611dea2239a0abd7ea8729b3
jcan03 <133265149+jcan03@users.noreply.github.com>
2024-10-11 13:27:02 -0700

    refactor some code, add password reset functionality, lower minmum sdk version, add internet permission to manifest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.comp3160project"
-        minSdk = 25
+        minSdk = 23
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
