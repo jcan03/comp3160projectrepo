fe1698df54130f4853994d3121a11b7c690a7f6b
Jumpshawt <heplaysvidogames@gmail.com>
2024-10-16 13:23:35 -0700

    Favorites adapter

---
+++ app/src/main/java/com/example/comp3160project/FavoritesAdapter.java
@@ -0,0 +1,44 @@
+package com.example.comp3160project;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class FavoritesAdapter extends RecyclerView.Adapter<RestaurantViewHolder> {
+
+    private Context context;
+    private List<Restaurant> favorites;
+
+    public FavoritesAdapter(Context context, List<Restaurant> favorites)
+    {
+        this.favorites = favorites;
+    }
+
+    @NonNull
+    @Override
+    public RestaurantViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_search_resturaunt_view, parent, false);
+        return new RestaurantViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RestaurantViewHolder holder, int position) {
+        Restaurant restaurant = favorites.get(position);
+        holder.name.setText(restaurant.getName());
+        holder.street.setText(restaurant.getStreet());
+        holder.distance.setText(String.valueOf(restaurant.getDistance()));
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return favorites.size();
+    }
+}
