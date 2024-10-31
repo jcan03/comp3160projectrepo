7b602a9c529503ce5be6419fc9b39d05f0ee0eb6
jcan03 <133265149+jcan03@users.noreply.github.com>
2024-10-31 16:36:07 -0700

    remove commented out code from restaurant file

--- app/src/main/java/com/example/comp3160project/Restaurant.java
+++ app/src/main/java/com/example/comp3160project/Restaurant.java
@@ -68,37 +68,4 @@ public class Restaurant {
         this.rating = rating;
     }
 
-    /* Stuff to implement later:
-    //Information
-    private static final int TAKEOUT = 485;
-    private static final int TAKEOUT_ONLY = 636;
-    private static final int DELIVERY = 835;
-    private static final int NO_DELIVERY = 442;
-    private static final int DINE_IN = 928;
-    private static final int DINE_IN_ONLY = 103;
-    private static final int DRIVE_THROUGH = 887;
-
-
-
-    private String name;
-    private String imgURL;
-
-    //Todo: Decide on a better way to store the resturaunt hours (I think there is a class for it)
-    private int[][] hours;
-    //Columns = days, 0 = Sunday 6 = Saturday:
-    //Rows = hours open {700,1630} = Open 7:00am-4:30pm {700,1330, 1430,1900} = 7am-1:30pm and 2:30pm-7:00pm (1h break)
-    private double[] coords;
-    private int[] options;
-
-
-    public Restaurant(String name, String imgURL, int[][] hours, double[] coords, int[] options) {
-        this.name = name;
-        this.imgURL = imgURL;
-        this.hours = hours;
-        this.coords = coords;
-        this.options = options;
-    }
-
-    */
-
 }
